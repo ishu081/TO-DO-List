5ba461ae4f9633cb6b24d08774b6d473fe13194f
ishu <shresthaishu222@gmail.com>
2025-05-29 23:42:50 +0545

    initial commit

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ let b;
 let a = document.getElementById("id1");
 let btn = document.getElementById("btn");
 let output = document.getElementById("output");
+let del = document.getElementById("delete")
+let clear= document.getElementById("clear");
+
+let c;
+let d;
+let e;
 function change()
 {
     console.log(a);
@@ -12,9 +18,30 @@ function change()
 }
 function click()
 {
-    output.innerText = b;
-    localStorage.setItem("task1",b);
-    
+    // output.innerText = b;
+    d=localStorage.length;
+    c=localStorage.setItem(d,b);
+    const data = JSON.parse(localStorage.getItem("myData"));
+    if(data && Array.isArray(data)){
+        data.forEach(item => {
+        const li = document.createElement("li");
+        output.appendChild(li);
+    });
+    else{
+        output.innerHTML= "<li>No Data <li>";
+    }
+}
+
+function Delete(){
+localStorage.removeItem(key);
+}
+function Clear()
+{
+    localStorage.clear();
 }
 a.addEventListener("input", change);
 btn.addEventListener("click",click);
+del.addEventListener("click",Delete);
+clear.addEventListener("click",Clear);
+
+
